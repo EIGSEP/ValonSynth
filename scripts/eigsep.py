88462c466f8c9d4d1de3e171fc26bbe72e9cf1c0
Christian Hellum Bye <chbye@berkeley.edu>
2025-06-16 14:46:11 -0700

    add arguments and some docs to scripts for programming eigsep valon

--- scripts/eigsep.py
+++ scripts/eigsep.py
@@ -1,26 +1,55 @@
+from argparse import ArgumentParser
+
 import valon_synth
 
-FREQ = 500.
+parser = ArgumentParser(description="Program Valon Synthesizer")
+parser.add_argument(
+    "--port", type=str, default="/dev/ttyUSB0", help="Serial port to use"
+)
+parser.add_argument(
+    "--use_ref", action="store_true", help="Use external 10 MHz reference"
+)
+parser.add_argument("-v", "--verbose", action="store_true")
+parser.add_argument(
+    "-f",
+    "--flash",
+    action="store_true",
+    default=True,
+    help="Flash the synthesizer after programming",
+)
+args = parser.parse_args()
 
-s = valon_synth.Synthesizer("/dev/ttyUSB0")
-#s.set_reference(10)
-s.set_ref_select(e_not_i=0)
-print("Input reference:", s.get_reference(), s.get_ref_select())
-flash = True
-for i in [0, 8]:
-    print("Channel %d"%i)
-    if s.set_frequency(i, FREQ):
-        print("Frequency set to %f MHz"%FREQ)
-        print("Frequency is %f MHz"%s.get_frequency(i))
+FREQ = 500.0  # MHz
+verbose = args.verbose
+
+s = valon_synth.Synthesizer(args.port)
+
+if args.use_ref:  # use external 10 MHz reference
+    s.set_reference(10)
+r = s.set_ref_select(e_not_i=int(args.use_ref))  # ext: 1, int: 0
+if not r:
+    raise RuntimeError("Failed to set reference select")
+
+if verbose:
+    print("Input reference:", s.get_reference())
+    if s.get_ref_select():
+        print("Using external reference")
     else:
-        print("Frequency set failed")
-        flash = False
-    if s.set_rf_level(i, 5):
+        print("Using internal reference")
+
+for synth in (valon_synth.SYNTH_A, valon_synth.SYNTH_B):
+    r = s.set_frequency(synth, FREQ)
+    if not r:
+        raise RuntimeError(f"Failed to set frequency for synth {synth}.")
+    if verbose:
+        print(f"Frequency set to {FREQ} MHz for synth {synth}")
+        print(f"Current frequency for synth {synth}: {s.get_frequency(synth)} MHz")
+    r = s.set_rf_level(synth, 5)
+    if not r:
+        raise RuntimeError(f"Failed to set RF level for synth {synth}")
+    if verbose:
         print("RF level set to 5 dBm")
-        print("RF level is %d dBm"%s.get_rf_level(i))
-    else:
-        print("RF level set failed")
-        flash = False
+        print(f"RF level is {s.get_rf_level(synth)} dBm.")
 
-if flash:
+if args.flash:
     s.flash()
